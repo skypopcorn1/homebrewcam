3b31faa422a574e9d3296f8d6ef8771a35062ea5
Jeremiah Flickinger <jeremiahflickinger@Jeremiahs-MacBook-Air.local>
2018-03-23 09:55:09 +1100

    more folder fixes and settings update

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -121,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = 'static'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, "static"),
-]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 
 MEDIA_URL = '/media/'
