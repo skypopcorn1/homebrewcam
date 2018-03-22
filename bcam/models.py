0e84fc4099e757060d000af6542e9d583dceac66
Jeremiah Flickinger <jeremiahflickinger@Jeremiahs-MacBook-Air.local>
2018-03-23 08:37:49 +1100

    added image model

--- bcam/models.py
+++ bcam/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+def upload_location (instance, filename):
+    return "%s" %( filename )
+
+class Images(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	image = models.ImageField(
+		upload_to=upload_location,
+		null=True,
+		blank=True,
+		)
