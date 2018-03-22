e6a9acede62fbc03f0483d184bec21690cdb6aed
Jeremiah Flickinger <jeremiahflickinger@Jeremiahs-MacBook-Air.local>
2018-03-23 09:02:16 +1100

    added image upload api

--- bcam/urls.py
+++ bcam/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from . import views
 
+app_name = 'bcam'
 
 urlpatterns = [
     url(r'^$', views.site_home, name="home"),
+	url(r'^upload/(?P<filename>[^/]+)$', views.FileUploadView.as_view(), name='upload')
     ]
--- myproject/settings/local.py
+++ myproject/settings/local.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'bcam'
 ]
 
@@ -120,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 # security settings
 CORS_REPLACE_HTTPS_REFERER      = False
